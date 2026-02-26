02c71baaace3f1693b34a4eacba6c5fa708d8a77
Arsenii Usenko <usenkoarsenii48@gmail.com>
2026-02-26 18:29:18 +0100

    OpenTab and About sections implemented (no responsive design yet)

--- frontend/src/components/About/About.tsx
+++ frontend/src/components/About/About.tsx
@@ -3,7 +3,16 @@ import "./About.css";
 export default function About() {
     return (
         <section className="about">
-
+            <div className="about__container container">
+                <h2 className="about__title">
+                    Findtrend helps you to increase your <br />
+                    productivity and reduce your computer's <br />
+                    memory load,{` `}
+                    <span className="about__title--muted">
+                        an application that can fulfill your daily browsing needs.
+                    </span>
+                </h2>
+            </div>
         </section>
     );
 }
--- frontend/src/components/OpenTab/OpenTab.tsx
+++ frontend/src/components/OpenTab/OpenTab.tsx
@@ -1,8 +1,34 @@
 import "./OpenTab.css";
+import arrow2 from "../../assets/icons/OpenTab/Arrow_2.svg";
+import photo1 from "../../assets/pictures/OpenTab/Photo_1.svg";
 
 export default function OpenTab() {
     return (
         <section className="openTab" id="open-tab">
+            <div className="openTab__container container">
+                <h2 className="openTab__title">Open new tabs is sh*t</h2>
+
+                <div className="openTab__cardWrap">
+                    <div className="openTab__hint openTab__hint--left">
+                        <img className="openTab__arrow openTab__arrow--left" src={arrow2} alt="" />
+                        <span className="openTab__hintText">solution for discover a trend</span>
+                    </div>
+
+                    <div className="openTab__card">
+                        <img className="openTab__photo" src={photo1} alt="" />
+                    </div>
+
+                    <div className="openTab__hint openTab__hint--right">
+                        <span className="openTab__hintText">You just need one tab now</span>
+                        <img className="openTab__arrow openTab__arrow--right" src={arrow2} alt="" />
+                    </div>
+                </div>
+
+                <p className="openTab__text">
+                    A solution for your browser tabs and don’t make your device get slower over time.
+                    Get ease and faster to discover a trend with just one tab.
+                </p>
+            </div>
         </section>
     );
 }
