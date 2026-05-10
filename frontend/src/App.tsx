5bb452c861d653b6d0c9941814d3757fc754e40f
Arsenii Usenko <usenkoarsenii48@gmail.com>
2026-05-10 13:53:28 +0200

    feat(open-tab): improve responsive layout and mobile hints positioning

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
     return (
         <>
         <Hero />
-        {/* <OpenTab /> */}
+        <OpenTab />
         {/* <About />
         <Startups />
         <Platforms />
